ace0f107dd7a924650dd2ebfc03b4081dfda2ebc
polar9527 <polar9527@163.com>
2019-01-02 11:36:57 +0800

    Initial commit

---
+++ src/main/java/com/polar9527/taco/TacoCloudApplication.java
@@ -0,0 +1,14 @@
+package com.polar9527.taco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/polar9527/taco/TacoCloudApplicationTests.java
@@ -0,0 +1,17 @@
+package com.polar9527.taco;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TacoCloudApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
