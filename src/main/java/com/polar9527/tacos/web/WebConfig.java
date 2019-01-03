96df1ab753b217fffc904c04c215c1d3091b509d
polar <polar9527@163.com>
2019-01-03 22:13:53 +0800

    Chapter 2

---
+++ src/main/java/com/polar9527/tacos/web/WebConfig.java
@@ -0,0 +1,15 @@
+package com.polar9527.tacos.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+
+}
